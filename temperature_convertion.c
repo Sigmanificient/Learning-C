8eab546d5e3bdb06ee342f0eab70e71ddee37666
sigmanificient <edhyjox@gmail.com>
2022-04-25 23:38:33 +0200

    :sparkles: Adding original Fahrenheit converter

---
+++ temperature_convertion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%1d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
